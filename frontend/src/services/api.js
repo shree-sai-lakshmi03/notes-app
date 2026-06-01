c58acfb206796fc7bf687d3fd05ab2a4488adc30
shree-sai-lakshmi03 <shreesailakshmi07@gmail.com>
2026-06-01 18:40:41 +0530

    update api url to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://notes-app-backend-tibu.onrender.com',
 });
 
 // Automatically attach token to every request
